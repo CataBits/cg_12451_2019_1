b74b58165f6a939fa951cb500fea2b96339ccef9
André Luferat <Andr Luferat>
2020-01-25 12:44:08 -0300

    Slice do Template

---
+++ _footer.php
@@ -0,0 +1,20 @@
+</main>
+    
+    <footer class="footer">
+        <a href="index.php" title="Ir para a página inicial"><i class="fas fa-fw fa-home"></i></a>
+        <div>
+            &copy; Copyright 2020 André Luferat
+            <div>
+                <a href="contatos.php">&nbsp;Faça contato&nbsp;</a>
+                |
+                <a href="privacidade.php">&nbsp;Privacidade&nbsp;</a>
+            </div>
+        </div>
+        <a href="#topo" title="Ir para o topo da página"><i class="fas fa-fw fa-arrow-alt-circle-up"></i></a>
+    </footer>
+
+</div>
+
+<script src="js/jquery.min.js"></script>
+</body>
+</html>
---
+++ _header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="img/logo01.jpg">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <title>Gatolândia - Sem arranhões</title>
+</head>
+<body>
+<a id="topo"></a>
+
+<div class="wrap">
+
+    <header class="header">
+        <a href="index.php"><img src="img/logo01.jpg" alt="Gatolândia" title="Home Page do Gatolândia"></a>
+        <h1>Gatolândia<small>Sem arranhões</small></h1>
+    </header>
+
+    <nav class="nav">
+        <a href="index.php" title="Vai para a home page"><i class="fas fa-fw fa-home"></i></a>
+        <a href="artigos.php"><i class="fas fa-fw fa-pen-nib"></i> Artigos</a>
+        <a href="sobre.php"><i class="fas fa-fw fa-info-circle"></i> Sobre</a>
+        <a href="procurar.php"><i class="fas fa-search"></i></a>
+    </nav>
+    
+    <main class="main">
